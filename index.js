80a60f6084f6111bafc7fc96417cd8df0e378a03
dkress59 <damian@nutcaze.de>
2020-08-09 01:24:34 +0200

    trying new buildpack for puppeteer

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
-});
+	res.header("Access-Control-Allow-Origin", "*")
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+	next()
+})
 
 app.get('/', async (req, res) => {
 	if (!req.query.url || !req.query.w || !req.query.h)
@@ -22,7 +22,7 @@ app.get('/', async (req, res) => {
 	if (req.query.darkMode)
 		await page.emulateMediaFeatures([{
 			name: 'prefers-color-scheme', value: 'dark'
-		}]);
+		}])
 
 	try {
 		await page.setViewport({
