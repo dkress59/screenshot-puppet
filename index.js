a94172879bb6865c52852038bcad31a4a2a05c3f
dkress59 <damian@nutcaze.de>
2020-08-09 14:54:52 +0200

    proper promises

--- index.js
+++ index.js
@@ -7,61 +7,63 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "https://www.damiankress.de")
+	res.header("Access-Control-Allow-Origin", "*")
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
 	next()
 })
 
-app.get('/', async (req, res) => {
-	if (!req.query.url || !req.query.w || !req.query.h)
-		return res.status(402).send({ error: 'Required param(s) missing.' })
+app.get('/', (req, res) => {
+	(async () => {
 
-	const browser = await puppeteer.launch({
-		defaultViewport: null,
-		args: [
-			'--no-sandbox',
-			'--disable-setuid-sandbox'
-		]
-	})
-	const page = await browser.newPage()
+		try {
 
-	if (req.query.darkMode)
-		await page.emulateMediaFeatures([{
-			name: 'prefers-color-scheme', value: 'dark'
-		}])
+			if (!req.query.url || !req.query.w || !req.query.h)
+				throw new Error('Required param(s) missing.')
 
-	try {
-		await page.setViewport({
-			width: parseInt(req.query.w),
-			height: parseInt(req.query.h)
-		})
+			const browser = await puppeteer.launch({
+				defaultViewport: null,
+				args: [
+					'--no-sandbox',
+					'--disable-setuid-sandbox'
+				]
+			})
+			const page = await browser.newPage()
 
-		if (req.query.cookie.length > 2)
-			await page.setCookie({
-				url: decodeURIComponent(req.query.url),
-				name: JSON.parse(req.query.cookie).key,
-				value: JSON.parse(req.query.cookie).val
+			await page.setViewport({
+				width: parseInt(req.query.w),
+				height: parseInt(req.query.h)
 			})
 
-		await page.goto(
-			decodeURIComponent(req.query.url)/* ,
+			if (req.query.darkMode)
+				await page.emulateMediaFeatures([{
+					name: 'prefers-color-scheme', value: 'dark'
+				}])
+
+			if (req.query.cookie.length > 2)
+				await page.setCookie({
+					url: decodeURIComponent(req.query.url),
+					name: JSON.parse(req.query.cookie).key,
+					value: JSON.parse(req.query.cookie).val
+				})
+
+			await page.goto(
+				decodeURIComponent(req.query.url)/* ,
 			{ waitUntil: 'domcontentloaded' } */
-		)
-		const screenshotBuffer = await page.screenshot()
-		const screenshot = screenshotBuffer.toString('base64')
+			)
+			const screenshotBuffer = await page.screenshot()
+			const screenshot = screenshotBuffer.toString('base64')
 
-		/* res.writeHead(200, {
-			'Content-Type': 'text/plain',
-			'Content-Length': screenshot.length
-		}) */
-		res.send(screenshot)
+			browser.close()
+			//return screenshot
+			return res.send(screenshot)
 
-	} catch (err) {
-		res.status(500).send('error')
-		console.error(err)
-	}
+		} catch (err) {
+			res.status(500).send({ error: err })
+			console.error(err)
+			return
+		}
 
-	return await browser.close()
+	})()
 })
 
 app.listen(process.env.PORT)
