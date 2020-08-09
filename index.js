87bf1ef0cc1f68932ec7b7ed9a547df297aba5ca
dkress59 <damian@nutcaze.de>
2020-08-09 18:13:20 +0200

    handling PromiseRjection

--- index.js
+++ index.js
@@ -12,59 +12,65 @@ app.use((req, res, next) => {
 	next()
 })
 
-app.get('/', async (req, res) => {
-	//(async () => {
+app.get('/', (req, res) => {
 
-	try {
-
-		if (!req.query.url || !req.query.w || !req.query.h)
-			throw 'Required param(s) missing.'
-
-		const browser = await puppeteer.launch({
-			defaultViewport: null,
-			args: [
-				'--no-sandbox',
-				'--disable-setuid-sandbox'
-			]
-		})
-		const page = await browser.newPage()
-
-		await page.setViewport({
-			width: parseInt(req.query.w),
-			height: parseInt(req.query.h)
-		})
-
-		if (req.query.darkMode)
-			await page.emulateMediaFeatures([{
-				name: 'prefers-color-scheme', value: 'dark'
-			}])
-
-		if (req.query.cookie.length > 2)
-			await page.setCookie({
-				url: decodeURIComponent(req.query.url),
-				name: JSON.parse(req.query.cookie).key,
-				value: JSON.parse(req.query.cookie).val
-			})
-
-		await page.goto(
-			decodeURIComponent(req.query.url)/* ,
-			{ waitUntil: 'domcontentloaded' } */
-		)
-		const screenshotBuffer = await page.screenshot()
-		const screenshot = screenshotBuffer.toString('base64')
-
-		await browser.close()
-		//return screenshot
-		return res.send({ img: screenshot })
-
-	} catch (err) {
-		console.error(err)
+	process.on("unhandledRejection", (reason, p) => {
+		console.error("Unhandled Rejection at: Promise", p, "reason:", reason)
 		browser.close()
-		return res.status(500).send({ error: err })
-		//return new Error(err)
-	}
+	})
 
-	//})()
+		(async () => {
+
+			try {
+
+				if (!req.query.url || !req.query.w || !req.query.h)
+					throw 'Required param(s) missing.'
+
+				const browser = await puppeteer.launch({
+					defaultViewport: null,
+					args: [
+						'--no-sandbox',
+						'--disable-setuid-sandbox'
+					]
+				})
+				const page = await browser.newPage()
+
+				page.setViewport({
+					width: parseInt(req.query.w),
+					height: parseInt(req.query.h)
+				})
+
+				if (req.query.darkMode)
+					await page.emulateMediaFeatures([{
+						name: 'prefers-color-scheme', value: 'dark'
+					}])
+
+				if (req.query.cookie.length > 2)
+					await page.setCookie({
+						url: decodeURIComponent(req.query.url),
+						name: JSON.parse(req.query.cookie).key,
+						value: JSON.parse(req.query.cookie).val
+					})
+
+				await page.goto(
+					decodeURIComponent(req.query.url)/* ,
+			{ waitUntil: 'domcontentloaded' } */
+				)
+				const screenshotBuffer = await page.screenshot()
+				const screenshot = screenshotBuffer.toString('base64')
+
+				browser.close()
+				//return screenshot
+				return res.send({ img: screenshot })
+
+			} catch (err) {
+				console.error(err)
+				browser.close()
+				return res.status(500).send({ error: err })
+				//return new Error(err)
+			}
+
+		})()
 })
 
 app.listen(process.env.PORT)
