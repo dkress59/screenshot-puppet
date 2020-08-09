a66bde77d70e9ba95c798fdbe570f0bf46fa45cd
dkress59 <damian@nutcaze.de>
2020-08-09 19:17:42 +0200

    cache header bug?

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json())
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
 	res.header("Access-Control-Allow-Origin", process.env.ALLOW_ACCESS)
-	res.header("Cache-Control", "public, max-age=2592000")
+	res.header("Cache-Control", "private, max-age=2592000")
 	res.type('application/json')
 	next()
 })
