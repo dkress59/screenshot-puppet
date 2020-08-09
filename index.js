446a9cf6d5213a6ce92a98ae152ece172b7c4987
dkress59 <damian@nutcaze.de>
2020-08-09 19:03:06 +0200

    cache header

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", process.env.ALLOW_ACCESS)
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+	res.header("Access-Control-Allow-Origin", process.env.ALLOW_ACCESS)
+	res.header("Cache-Control", "public, max-age=2592000")
+	res.type('application/json')
 	next()
 })
 
