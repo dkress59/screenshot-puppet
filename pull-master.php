cc34f79271b6736e5a1b8fb0d42354a16292e759
dkress59 <28515387+dkress59@users.noreply.github.com>
2020-08-23 00:11:40 +0200

    pipeline test

--- pull-master.php
+++ pull-master.php
@@ -1,11 +1,15 @@
 <?php
 echo "<h1>PIPELINE</h1><br />\n";
-exec('killall node');
+
+exec('killall -9 node');
 echo "node.js killed.<br />\n";
+
 exec('git pull');
 echo "git repo pulled.<br />\n";
+
 exec('npm install');
 exec('npm i puppeteer --unsafe-perm=true');
 echo "npm packages updated.<br />\n";
+
 exec('node index.js &');
 echo "node.js restarted.<br />\n";
