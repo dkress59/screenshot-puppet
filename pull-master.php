e224e313311226a3eda322714b434d65945394f4
dkress59 <28515387+dkress59@users.noreply.github.com>
2020-08-23 00:27:24 +0200

    better exec

--- pull-master.php
+++ pull-master.php
@@ -1,15 +1,15 @@
 <?php
 echo "<h1>PIPELINE</h1><br />\n";
 
-shell_exec('killall -9 node');
+`killall -9 node`;
 echo "node.js killed.<br />\n";
 
-shell_exec('git pull');
+`git pull`;
 echo "git repo pulled.<br />\n";
 
-shell_exec('npm install');
-shell_exec('npm i puppeteer --unsafe-perm=true');
+`npm install`;
+`npm i puppeteer --unsafe-perm=true`;
 echo "npm packages updated.<br />\n";
 
-shell_exec('node index.js &');
+`node index.js &`;
 echo "node.js restarted.<br />\n";
