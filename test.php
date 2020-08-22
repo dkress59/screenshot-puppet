dc701f41b3126cec7df14c3c1cdfc9decf308b54
dkress59 <28515387+dkress59@users.noreply.github.com>
2020-08-23 01:02:04 +0200

    reaal test

--- test.php
+++ test.php
@@ -1,2 +1,13 @@
 <?php
-echo 'exec fetch test update 1000000';
+echo "exec fetch test update 1000000<br />\n";
+
+if (function_exists('exec')) {
+	echo "exec is enabled<br />\n";
+} else {
+	echo "exec is disabled<br />\n";
+}
+if (function_exists('shell_exec')) {
+	echo "shell_exec is enabled<br />\n";
+} else {
+	echo "shell_exec is disabled<br />\n";
+}
