e1e18bcd01b6a697a0c6d560af38aff610a4d9cf
dkress59 <28515387+dkress59@users.noreply.github.com>
2020-08-23 00:54:16 +0200

    cd + exec chain

--- pull-master.php
+++ pull-master.php
@@ -1,20 +1,22 @@
 <?php
 echo "<h1>PIPELINE</h1><br />\n";
 
-echo `killall -9 node`;
+echo `cd /var/www/screenshot-puppet && git fetch --all && git checkout --force "origin/master" && npm install && killall -9 node && node index.js &`;
+
+//echo `killall -9 node`;
 echo "<br />
 	node.js killed.<br />\n";
 
-echo `git fetch --all`;
-echo `git checkout --force "origin/master"`;
+//echo `git fetch --all`;
+//echo `git checkout --force "origin/master"`;
 echo "<br />
 	git repo pulled.<br />\n";
 
-echo `npm install`;
-echo `npm i puppeteer --unsafe-perm=true`;
+//echo `npm install`;
+//echo `npm i puppeteer --unsafe-perm=true`;
 echo "<br />
 	npm packages updated.<br />\n";
 
-echo `node index.js &`;
+//echo `node index.js &`;
 echo "<br />
 	node.js restarted.<br />\n";
