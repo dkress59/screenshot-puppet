6c542bafc38048661ae82b7affd603afbb0e7855
dkress59 <28515387+dkress59@users.noreply.github.com>
2020-08-23 10:25:17 +0200

    .

--- pull-master.php
+++ pull-master.php
@@ -1,5 +1,5 @@
 <?php
 exec('cd /var/www/screenshot-puppet', $out);
-exec('git    ALL = (www-data) /usr/bin/git fetch --all', $out);
-exec('git    ALL = (www-data) /usr/bin/git checkout --force "origin/master"', $out);
+exec('git fetch --all', $out);
+exec('git checkout --force "origin/master"', $out);
 print_r($out);
