20ece1cb4065d8cb708825d97569b458c8d81927
dkress59 <28515387+dkress59@users.noreply.github.com>
2020-08-23 10:24:00 +0200

    ??

--- pull-master.php
+++ pull-master.php
@@ -1,3 +1,5 @@
 <?php
-exec('git    ALL = (www-data) /usr/bin/git pull', $out);
+exec('cd /var/www/screenshot-puppet', $out);
+exec('git    ALL = (www-data) /usr/bin/git fetch --all', $out);
+exec('git    ALL = (www-data) /usr/bin/git checkout --force "origin/master"', $out);
 print_r($out);
