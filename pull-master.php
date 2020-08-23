ac68f64593009586043a7c7d8dd0d74aad5f0bae
dkress59 <28515387+dkress59@users.noreply.github.com>
2020-08-23 10:06:46 +0200

    advanced output

--- pull-master.php
+++ pull-master.php
@@ -1,21 +1,36 @@
 <?php
-exec('cd /var/www/screenshot-puppet');
+exec('cd /var/www/screenshot-puppet', $output);
 echo "<h1>PIPELINE</h1><br />\n";
+print_r($output);
+echo "<br />\n";
+unset($output);
 
-exec('killall -9 node');
+exec('killall -9 node', $output);
 echo "<br />
-	node.js killed.<br />\n";
+	node.js killed:<br />\n";
+print_r($output);
+echo "<br />\n";
+unset($output);
 
-exec('git fetch --all');
-exec('git checkout --force "origin/master"');
+exec('git fetch --all', $output);
+exec('git checkout --force "origin/master"', $output);
 echo "<br />
-	git repo pulled.<br />\n";
+	git repo pulled:<br />\n";
+print_r($output);
+echo "<br />\n";
+unset($output);
 
-exec('npm install');
-exec('npm i puppeteer --unsafe-perm=true');
+exec('npm install', $output);
+exec('npm i puppeteer --unsafe-perm=true', $output);
 echo "<br />
-	npm packages updated.<br />\n";
+	npm packages updated:<br />\n";
+print_r($output);
+echo "<br />\n";
+unset($output);
 
-exec('node index.js &');
+exec('node index.js &', $output);
 echo "<br />
-	node.js restarted.<br />\n";
+	node.js restarted:<br />\n";
+print_r($output);
+echo "<br />\n";
+unset($output);
