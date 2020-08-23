81d491d939f65ad772317de4924ea061e9575e03
dkress59 <28515387+dkress59@users.noreply.github.com>
2020-08-23 10:18:09 +0200

    test

--- pull-master.php
+++ pull-master.php
@@ -1,8 +1,8 @@
 <?php
-exec('cd /var/www/screenshot-puppet && git pull', $output);
+/* exec('cd /var/www/screenshot-puppet && git pull', $output);
 echo "<h1>PIPELINE</h1><br />\n";
 print_r($output);
 echo "<br />\n";
-unset($output);
+unset($output); */
 
-echo `cd /var/www/screenshot-puppet && git pul`;
+echo `cd /var/www/screenshot-puppet && git pull`;
