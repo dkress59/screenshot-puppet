ac63ab0735b285862cd231caa850576070d395ad
dkress59 <28515387+dkress59@users.noreply.github.com>
2020-08-25 23:01:48 +0200

    yapt

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ const cache = async (req, res, next) => {
 		case 'GET':
 			const image = req.query
 			if (!image || !Object.entries(req.query).length)
-				return res.status(400).send({ error: 'Required param(s) missing.' })
+				return res.status(400).send({ error: 'Required param(s) missing. PIPELINE' })
 
 			const { w, h, link, title } = image
 			const cacheId = `${link}-${w}x${h}`
