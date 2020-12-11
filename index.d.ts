0116e71821f936e5ca182fa95af82f6bb44a4f78
dkress59 <28515387+dkress59@users.noreply.github.com>
2020-12-11 21:44:56 +0100

    test: module declaration from src

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,1 @@
-import { Screenshot as oScreenshot } from './src/util/Screenshot'
-import { PuppetOptions as oPuppetOptions } from './src/PuppetOptions'
-
-declare module 'screenshotr' {
-	export class Screenshot extends oScreenshot {}
-	export type PuppetOptions = oPuppetOptions
-	export default function screenshotR(options?: PuppetOptions | 'get' | 'post'): (req: Request, res: Response) => Promise<void> {
-		return screenshotR(options)
-	}
-}
+export * from './src/index'
